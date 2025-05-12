abd5691db0884f2b5536b6daaefaf88047bd4f17
Laura Szmulewicz <63259842+lauraszm@users.noreply.github.com>
2025-05-12 19:34:38 -0300

    Revert "Santifbouhid patch 1"

---
+++ lib/presentaciones/screens/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
