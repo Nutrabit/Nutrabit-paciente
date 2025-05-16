2347e55782962580fd02341bc73c610f96a6c647
Axelchaikh <axelchaikh@gmail.com>
2025-05-16 19:41:08 -0300

    Agregado de SigleChilScrollView a panatallas de Carousel y amIPatient

--- lib/widgets/welcome/welcome.dart
+++ lib/widgets/welcome/welcome.dart
@@ -7,37 +7,43 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     var freeSpace = screenHeight * 0.35;
-    return Scaffold(
+    return Scaffold( 
+      
         backgroundColor: Color.fromRGBO(253, 238, 219, 1),
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.15,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: freeSpace),
-                Text(
-                  '¡Bienvenido',
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: Color.fromRGBO(227, 126, 142, 1),
-                    fontWeight: FontWeight.w400,
-                  ),
+        body: SafeArea( child: 
+          SingleChildScrollView( child: 
+            Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.15,
                 ),
-                Text(
-                  'a tu nuevo estilo de vida!',
-                  style: TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.w900,
-                    color: Color.fromRGBO(153, 159, 127, 1),
-                  ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: freeSpace),
+                    Text(
+                      '¡Bienvenido',
+                      style: TextStyle(
+                        fontSize: 45,
+                        color: Color.fromRGBO(227, 126, 142, 1),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      'a tu nuevo estilo de vida!',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.w900,
+                        color: Color.fromRGBO(153, 159, 127, 1),
+                      ),
+                    ),
+                    SizedBox(height: freeSpace),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
         ),
-      );
+      ),
+    );
   }
 }
